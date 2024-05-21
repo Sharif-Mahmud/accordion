738d7b69fe8f79283f8a46634a870daa82d9b750
Sharif-Mahmud <sharif230@gmail.com>
2024-05-21 19:45:49 +0600

    this is an accordion like bootstrap accordion

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const accordion_header = document.querySelectorAll(".accordion_header");
+
+accordion_header.forEach(item => {
+  item.addEventListener("click", function(){
+      item.classList.toggle("active");
+
+      if(item.classList.contains("active")){
+        item.nextElementSibling.style.height =
+          item.nextElementSibling.scrollHeight + "px";
+      }else{
+        item.nextElementSibling.style.height = "0px";
+      }
+      
+  });
+});
+
